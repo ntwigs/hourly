7c3bdcaee5f4b2aa28e53e2350f3900a7d389850
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-27 21:01:07 +0200

    feat: add blocks to start page

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -1,3 +1,15 @@
+import { HeaderBlock } from '../../blocks/header-block'
+import { InputBlock } from '../../blocks/input-block'
+import { ItemBlock } from '../../blocks/item-block'
+import { Layout } from '../../components/layout'
+
 export const Start = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <HeaderBlock title="Hourly" />
+      <InputBlock title="Rate per hour" />
+      <InputBlock title="Price per burger" />
+      <ItemBlock items={['burger', 'potatoe', 'keyboard']} />
+    </Layout>
+  )
 }
