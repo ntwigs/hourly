cfac4bd1bdddc5e6e7ac83e8f18ab2030a00671d
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-27 20:41:24 +0200

    feat: initialize theme

---
+++ src/theme/index.tsx
@@ -0,0 +1,22 @@
+const sizes = [16, 24]
+const spacing = [0, 8, 16, 24]
+const colors = {
+  black: ['#333333', '#CFCFCF', '#FFFFFF'],
+  red: ['#FFAAA7'],
+  yellow: ['#FFD3B4'],
+  green: ['#D5ECC2'],
+  blue: ['#98DDCA'],
+}
+const weight = [400, 700, 900]
+const fonts = {
+  primary: 'sans-serif, serif',
+  secondary: 'serif',
+}
+
+export const theme = {
+  sizes,
+  spacing,
+  colors,
+  weight,
+  fonts,
+}
