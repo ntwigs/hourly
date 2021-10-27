4eef91a41a021eb6373860d1c2e390f165a2ed1f
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-27 20:42:14 +0200

    feat: add input block

---
+++ src/blocks/input-block/index.tsx
@@ -0,0 +1,13 @@
+import { Section } from '../../components/section'
+
+interface Props {
+  title: string
+}
+
+export const InputBlock = ({ title }: Props): JSX.Element => {
+  return (
+	<Section>
+		<Title>
+	</Section>
+	)
+}
