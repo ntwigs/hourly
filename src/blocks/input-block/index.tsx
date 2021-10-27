6b2dc08030e8ffb9612a6fece80f1b03f0f68257
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-27 21:00:02 +0200

    feat: create input block

--- src/blocks/input-block/index.tsx
+++ src/blocks/input-block/index.tsx
@@ -1,13 +1,22 @@
 import { Section } from '../../components/section'
+import { InputTitle } from '../../components/typography'
+import { Input } from '../../components/input'
+import { ChangeEvent, useState } from 'react'
 
 interface Props {
   title: string
 }
 
 export const InputBlock = ({ title }: Props): JSX.Element => {
+  const [value, setValue] = useState<string>()
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value)
+
   return (
-	<Section>
-		<Title>
-	</Section>
-	)
+    <Section>
+      <InputTitle>{title}</InputTitle>
+      <Input placeholder="$" onChange={onChange} value={value} />
+    </Section>
+  )
 }
