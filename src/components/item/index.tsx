4807ba106707fca12eb1e0384885dfaa4ff1ec6c
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-29 21:55:45 +0200

    feat: items with aspect-ratio

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,12 +1,19 @@
 import styled from 'styled-components'
 
-interface Props {
+interface ContainerProps {
   isSelected?: boolean
+  color: string
 }
 
-export const Item = styled.button<Props>(({ isSelected }) => ({
-  width: 45,
-  height: 45,
-  borderRadius: 16,
-  filter: isSelected ? 'saturate(0.2)' : 'saturate(1)',
-}))
+export const Item = styled.button<ContainerProps>(
+  ({ theme, isSelected, color }) => ({
+    background: color,
+    borderRadius: theme.sizes[0],
+    filter: isSelected ? 'saturate(1)' : 'saturate(0.6)',
+    border: 'none',
+    width: '100%',
+    aspectRatio: '1 / 1',
+    cursor: 'pointer',
+    padding: theme.spacing[1],
+  })
+)
