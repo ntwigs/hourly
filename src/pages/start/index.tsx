cec348eebf238bd3e62f7618ed13b9cdd979ab5c
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-29 21:56:31 +0200

    fix: use items data

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -2,6 +2,7 @@ import { HeaderBlock } from '../../blocks/header-block'
 import { InputBlock } from '../../blocks/input-block'
 import { ItemBlock } from '../../blocks/item-block'
 import { Layout } from '../../components/layout'
+import { items } from '../../data/items'
 
 export const Start = () => {
   return (
@@ -9,7 +10,7 @@ export const Start = () => {
       <HeaderBlock title="Hourly" />
       <InputBlock title="Rate per hour" />
       <InputBlock title="Price per burger" />
-      <ItemBlock items={['burger', 'potatoe', 'keyboard']} />
+      <ItemBlock items={items} />
     </Layout>
   )
 }
