10d3592953c91f009d4d2fb9564555a4f733ea63
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-29 21:55:32 +0200

    feat: add input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -3,5 +3,9 @@ import styled from 'styled-components'
 export const Input = styled.input(({ theme }) => ({
   width: '100%',
   height: 45,
-  outline: `2px solid ${theme.colors.black[1]}`,
+  border: `2px solid ${theme.colors.black[1]}`,
+  borderRadius: theme.sizes[0],
+  padding: theme.spacing[1],
+  color: theme.colors.black[0],
+  fontWeight: theme.weight[2],
 }))
