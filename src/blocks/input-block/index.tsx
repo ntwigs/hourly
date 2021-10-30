258ccd934ab6a4ab8c10f246c1ad55cc08309a9a
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-30 13:02:20 +0200

    feat: add symbol to input

--- src/blocks/input-block/index.tsx
+++ src/blocks/input-block/index.tsx
@@ -1,15 +1,70 @@
 import { Section } from '../../components/section'
 import { InputTitle } from '../../components/typography'
 import { Input } from '../../components/input'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, RefObject, useEffect, useRef, useState } from 'react'
 import { Spacer } from '../../components/spacer'
+import styled from 'styled-components'
+
+const Span = styled.span(({ theme }) => ({
+  fontSize: theme.sizes[0],
+  fontWeight: theme.weight[1],
+  visibility: 'hidden',
+  height: 0,
+  position: 'absolute',
+}))
+
+interface SymbolProps {
+  left: number
+}
+
+const Symbol = styled.span<SymbolProps>(({ left, theme }) => ({
+  position: 'absolute',
+  fontSize: theme.sizes[0],
+  fontWeight: theme.weight[1],
+  left: left + theme.spacing[1],
+  color: theme.colors.black[0],
+  paddingLeft: theme.spacing[2],
+}))
+
+const useWidth = (value?: string): [RefObject<HTMLElement>, number] => {
+  const [width, setWidth] = useState<number>(0)
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const current = ref.current
+
+    if (current) {
+      const rect = current.getBoundingClientRect()
+      setWidth(rect.width)
+    }
+  }, [value])
+
+  return [ref, width]
+}
+
+const InputContainer = styled.div({
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+})
 
 interface Props {
   title: string
+  defaultValue: number
+  max?: number
 }
 
-export const InputBlock = ({ title }: Props): JSX.Element => {
+export const InputBlock = ({
+  title,
+  defaultValue,
+  max,
+}: Props): JSX.Element => {
   const [value, setValue] = useState<string>()
+  const [ref, width] = useWidth(value ?? '')
+
+  useEffect(() => {
+    setValue(`${defaultValue}`)
+  }, [defaultValue])
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) =>
     setValue(e.target.value)
@@ -20,7 +75,11 @@ export const InputBlock = ({ title }: Props): JSX.Element => {
         <Spacer size={2}>
           <InputTitle>{title}</InputTitle>
         </Spacer>
-        <Input placeholder="$" onChange={onChange} value={value} />
+        <InputContainer>
+          <Input onChange={onChange} value={value} maxLength={max} />
+          {(value ?? 0) > 0 && <Symbol left={width}>$</Symbol>}
+        </InputContainer>
+        <Span ref={ref}>{value}</Span>
       </Spacer>
     </Section>
   )
