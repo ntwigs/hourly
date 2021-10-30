b623bdb68a2d8846870e3e1d78494e044320715b
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-30 13:03:24 +0200

    fix: animateable grid

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,6 +1,7 @@
+import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
-export const Grid = styled.section(({ theme }) => ({
+export const Grid = styled(motion.section)(({ theme }) => ({
   display: 'grid',
   gridTemplateColumns: 'repeat(4, 1fr)',
   gap: theme.spacing[2],
