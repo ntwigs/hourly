f5d403dff2c366412939d2893a79ee1214cd5d50
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-30 13:03:51 +0200

    feat: layout restructuring

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -1,16 +1,31 @@
+import { useState } from 'react'
 import { HeaderBlock } from '../../blocks/header-block'
 import { InputBlock } from '../../blocks/input-block'
 import { ItemBlock } from '../../blocks/item-block'
 import { Layout } from '../../components/layout'
-import { items } from '../../data/items'
+import { Spacer } from '../../components/spacer'
+import { Item, items } from '../../data/items'
 
-export const Start = () => {
+const DEFAULT_HOURLY_RATE = 100
+
+export const Start = (): JSX.Element => {
+  const [selection, setSelection] = useState<Item>(items[0])
+
   return (
     <Layout>
       <HeaderBlock title="Hourly" />
-      <InputBlock title="Rate per hour" />
-      <InputBlock title="Price per burger" />
-      <ItemBlock items={items} />
+      <InputBlock
+        max={6}
+        title="Rate per hour"
+        defaultValue={DEFAULT_HOURLY_RATE}
+      />
+      <InputBlock
+        title={`Price per ${selection.name}`}
+        defaultValue={selection.price}
+        max={6}
+      />
+      <Spacer size={4} />
+      <ItemBlock items={items} selection={selection} onClick={setSelection} />
     </Layout>
   )
 }
