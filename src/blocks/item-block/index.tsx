5b7456f77be4b2a6230427d6a02e621c0c0ce26f
ntwigs <oscar.cv.nordquist@gmail.com>
2021-10-31 15:43:08 +0100

    feat: item animation

--- src/blocks/item-block/index.tsx
+++ src/blocks/item-block/index.tsx
@@ -11,18 +11,41 @@ interface Props {
 }
 
 const parentVariants: Variants = {
-  enter: {
+  mount: {
     transition: {
-      staggerChildren: 0.02,
+      staggerChildren: 0.2,
     },
   },
 }
 
 const itemVariants: Variants = {
-  enter: {
+  mount: {
     opacity: 1,
     y: 0,
+    rotate: 0,
   },
+  unmount: {
+    opacity: 0,
+    y: 50,
+    rotate: 180,
+  },
+  hover: {
+    scale: 1.2,
+  },
+}
+
+interface ColorIndex {
+  index: number
+}
+
+const getColorIndex = ({ index }: ColorIndex): number => {
+  const MAX = 4
+  const a = index % MAX
+  const b = index % 8
+
+  console.log(a, b)
+
+  return index % 4
 }
 
 export const ItemBlock = ({
@@ -34,19 +57,16 @@ export const ItemBlock = ({
   const getColor = useColor()
 
   return (
-    <Grid animate="enter" variants={parentVariants}>
+    <Grid variants={parentVariants}>
       {items.map(({ name, image, price }, index) => (
         <Item
+          whileHover="hover"
           as={motion.div}
           key={name}
           isSelected={selection.name === name}
           onClick={selectItem({ name, image, price })}
-          color={getColor({ color: index % 4 })}
+          color={getColor({ color: getColorIndex({ index }) })}
           variants={itemVariants}
-          initial={{
-            opacity: 0,
-            y: 100,
-          }}
         >
           <img src={image} alt={name} />
         </Item>
