a120b309f2c63737e9e218e017e8ededc16e0436
Oscar Nordquist <oscar.nordquist@beanloop.se>
2021-11-01 21:16:52 +0100

    style: prepare layout for circle

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,5 +3,9 @@ import styled from 'styled-components'
 export const Layout = styled.main(({ theme }) => ({
   width: 235,
   padding: theme.spacing[3],
-  borderRadius: theme.sizes[0]
+  borderRadius: theme.sizes[0],
+  background: theme.colors.black[3],
+  position: 'relative',
+  overflow: 'hidden',
+  zIndex: 1
 }))
