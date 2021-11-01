169223aa73a544cdf675a634b5b90ce728b80c7a
Oscar Nordquist <oscar.nordquist@beanloop.se>
2021-11-01 19:47:33 +0100

    feat: add error border

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,12 +1,20 @@
 import styled from 'styled-components'
 
-export const Input = styled.input(({ theme }) => ({
+interface Props {
+  isInvalid?: boolean
+}
+
+export const Input = styled.input<Props>(({ theme, isInvalid }) => ({
   width: '100%',
   height: 45,
-  border: `2px solid ${theme.colors.black[1]}`,
+  border: `2px solid ${isInvalid ? theme.colors.red[0] : theme.colors.black[1]}`,
   borderRadius: theme.sizes[0],
   padding: theme.spacing[2],
+  paddingLeft: theme.spacing[4],
   color: theme.colors.black[0],
   fontWeight: theme.weight[2],
   fontSize: theme.sizes[0],
+  outline: 'none',
+  fontFamily: theme.fonts.primary,
+  transition: 'border 250ms'
 }))
