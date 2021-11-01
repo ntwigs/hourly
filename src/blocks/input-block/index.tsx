4ef588fd2ff66b3fa90a137c2957e5cd8f585c1e
Oscar Nordquist <oscar.nordquist@beanloop.se>
2021-11-01 19:47:18 +0100

    feat: better currency symbol

--- src/blocks/input-block/index.tsx
+++ src/blocks/input-block/index.tsx
@@ -1,48 +1,21 @@
 import { Section } from '../../components/section'
-import { InputTitle } from '../../components/typography'
+import { InputTitle, InputValue } from '../../components/typography'
 import { Input } from '../../components/input'
-import { ChangeEvent, RefObject, useEffect, useRef, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { Spacer } from '../../components/spacer'
 import styled from 'styled-components'
 import { AnimatedText } from '../../components/animated-text'
 import { motion, Variants } from 'framer-motion'
 
-const Span = styled.span(({ theme }) => ({
-  fontSize: theme.sizes[0],
-  fontWeight: theme.weight[1],
-  visibility: 'hidden',
-  height: 0,
-  position: 'absolute',
-}))
-
-interface SymbolProps {
-  left: number
-}
-
-const Symbol = styled.span<SymbolProps>(({ left, theme }) => ({
+const Symbol = styled(InputValue)(({ theme }) => ({
   position: 'absolute',
   fontSize: theme.sizes[0],
-  fontWeight: theme.weight[1],
-  left: left + theme.spacing[1],
+  fontWeight: theme.weight[2],
+  left: theme.spacing[1],
   color: theme.colors.black[0],
   paddingLeft: theme.spacing[2],
+  fontFamily: theme.fonts.primary
 }))
-
-const useWidth = (value?: string): [RefObject<HTMLElement>, number] => {
-  const [width, setWidth] = useState<number>(0)
-  const ref = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const current = ref.current
-
-    if (current) {
-      const rect = current.getBoundingClientRect()
-      setWidth(rect.width)
-    }
-  }, [value])
-
-  return [ref, width]
-}
 
 const InputContainer = styled(motion.div)({
   position: 'relative',
@@ -81,7 +54,6 @@ export const InputBlock = ({
   max,
 }: Props): JSX.Element => {
   const [value, setValue] = useState<string>(`${defaultValue}`)
-  const [ref, width] = useWidth(value)
 
   useEffect(() => {
     if (value !== defaultValue) {
@@ -93,7 +65,7 @@ export const InputBlock = ({
   const onChange = (e: ChangeEvent<HTMLInputElement>) =>
     setValue(e.target.value)
 
-  const shouldRenderSuffix = value.length > 0 && width > 0
+  const isValid = value.length > 0 && typeof +value === 'number'
 
   return (
     <Section>
@@ -104,10 +76,9 @@ export const InputBlock = ({
           </InputTitle>
         </Spacer>
         <InputContainer variants={inputVariants}>
-          <Input onChange={onChange} value={value} maxLength={max} />
-          {shouldRenderSuffix && <Symbol left={width}>$</Symbol>}
+          <Input onChange={onChange} value={value} maxLength={max} isInvalid={!isValid} />
+          <Symbol>$</Symbol>
         </InputContainer>
-        <Span ref={ref}>{value}</Span>
       </Spacer>
     </Section>
   )
