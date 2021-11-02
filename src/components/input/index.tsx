ed837fe7f5c42f05a530242633cf6ba97798c0c2
Oscar Nordquist <oscar.nordquist@beanloop.se>
2021-11-02 10:40:02 +0100

    style: better input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Input = styled.input<Props>(({ theme, isInvalid }) => ({
   width: '100%',
   height: 45,
-  border: `2px solid ${isInvalid ? theme.colors.red[0] : theme.colors.black[1]}`,
+  border: `2px solid ${isInvalid ? theme.colors.red[0] : theme.colors.black[3]}`,
   borderRadius: theme.sizes[0],
   padding: theme.spacing[2],
   paddingLeft: theme.spacing[4],
@@ -16,5 +16,6 @@ export const Input = styled.input<Props>(({ theme, isInvalid }) => ({
   fontSize: theme.sizes[0],
   outline: 'none',
   fontFamily: theme.fonts.primary,
-  transition: 'border 250ms'
+  transition: 'border 250ms',
+  boxShadow: '0 4px 14px 0 rgba(0, 10, 91, 0.1)'
 }))
