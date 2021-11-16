a5e3a118cc5c09cf3b405886499e520df5d5f142
ntwigs <oscar.cv.nordquist@gmail.com>
2021-11-16 21:10:53 +0100

    style: fixed content icon size

--- src/components/content-icon/index.tsx
+++ src/components/content-icon/index.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import { Item } from '../item'
 
 export const ContentIcon = styled(Item)(({ theme }) => ({
+  minWidth: theme.sizes[2],
+  maxWidth: theme.sizes[2],
   width: theme.sizes[2],
   background: 'transparent',
   cursor: 'auto',
