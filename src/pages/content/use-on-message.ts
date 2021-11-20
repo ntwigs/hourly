53fa66672f1ae014f0310f46a974f3544d07b0a7
ntwigs <oscar.cv.nordquist@gmail.com>
2021-11-20 20:15:15 +0100

    feat: use on message

---
+++ src/pages/content/use-on-message.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+import { isNumber } from '../../utils/is-number'
+import { Event } from '../../utils/storage'
+
+interface OnMessage {
+  item: Event['selection']
+  setItem: (item: Event['selection']) => void
+  setRate: (rate: Event['rate']) => void
+  setCost: (cost: Event['cost']) => void
+}
+
+export const useOnMessage = ({
+  item,
+  setItem,
+  setRate,
+  setCost,
+}: OnMessage) => {
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener(({ selection, rate, cost }: Event) => {
+      if (selection && selection.name !== item?.name) {
+        setItem(selection)
+      }
+      if (isNumber(rate)) {
+        setRate(rate)
+      }
+      if (isNumber(cost)) {
+        setCost(cost)
+      }
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+}
