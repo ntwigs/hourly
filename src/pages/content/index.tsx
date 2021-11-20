1f5ed72771991a3a290479e3101ad65865f07a9a
ntwigs <oscar.cv.nordquist@gmail.com>
2021-11-20 20:15:33 +0100

    refactor; content items

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,82 +1,34 @@
 import { motion, Variants } from 'framer-motion'
 import { useTheme } from 'styled-components'
 import { ContentIcon } from '../../components/content-icon'
-import { Item } from '../../data/items'
 import { Icon } from '../../components/icons'
-import { useMemo, useEffect } from 'react'
 import { useStorageState } from '../../hooks/use-storage-state'
-import { useTimeObserver } from '../../hooks/use-time-observer'
-
-interface UseAmount {
-  time?: string
-  rate?: string
-  cost?: string
-}
-
-const isString = (value: unknown): value is string => typeof value === 'string'
-const isNumber = (value: unknown): value is number =>
-  typeof +(value as string) === 'number'
-
-const getAmount = ({ time, rate, cost }: UseAmount): string => {
-  if (!isString(time)) return ''
-  if (!isNumber(rate)) return ''
-  if (!isNumber(cost)) return ''
-
-  const [hours, minutes, seconds] = time.split(':')
-  const decimalHours = +hours + +minutes / 60 + +seconds / 3600
-
-  const salary = +rate * decimalHours
-  return (salary / +cost).toFixed(0)
-}
-
-const useAmount = ({ time, rate, cost }: UseAmount): string => {
-  const amount = useMemo(
-    () => getAmount({ time, rate, cost }),
-    [time, rate, cost]
-  )
-  return amount
-}
+import { useTimeObserver } from './use-time-observer'
+import { useOnMessage } from './use-on-message'
+import { Event } from '../../utils/storage'
+import { useAmount } from './use-amount'
 
 interface Props {
-  timeObserver: (fn: MutationCallback) => void
+  timeObserver: (fn: MutationCallback) => MutationObserver
   defaultTime?: string
   index?: number
 }
+
 export const Content = ({
   timeObserver,
   defaultTime = '',
   index = 0,
 }: Props): JSX.Element | null => {
   const theme = useTheme()
-  const [rate, setRate] = useStorageState<string>({ selector: 'rate' })
-  const [cost, setCost] = useStorageState<string>({ selector: 'cost' })
-  const [item, setItem] = useStorageState<Item>({ selector: 'selection' })
+  const [rate, setRate] = useStorageState<Event['rate']>({ selector: 'rate' })
+  const [cost, setCost] = useStorageState<Event['cost']>({ selector: 'cost' })
+  const [item, setItem] = useStorageState<Event['selection']>({
+    selector: 'selection',
+  })
   const time = useTimeObserver({ defaultTime, timeObserver })
   const amount = useAmount({ time, rate, cost })
 
-  useEffect(() => {
-    chrome.runtime.onMessage.addListener(
-      ({
-        selection,
-        rate,
-        cost,
-      }: {
-        selection: Item
-        rate: string
-        cost: string
-      }) => {
-        if (selection && selection.name !== item?.name) {
-          setItem(selection)
-        }
-        if (rate && rate.length > 0 && +rate > 0) {
-          setRate(rate)
-        }
-        if (cost && cost.length > 0 && +cost > 0) {
-          setCost(cost)
-        }
-      }
-    )
-  }, [])
+  useOnMessage({ item, setItem, setRate, setCost })
 
   if (!item || !rate || !cost) {
     return null
