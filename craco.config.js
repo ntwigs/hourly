07da3db2a52a5bdf2ed6c0199af0a84eb7b544be
ntwigs <oscar.cv.nordquist@gmail.com>
2021-12-01 23:48:58 +0100

    feat: remove scripts from popup build

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,41 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   webpack: {
+    plugins: {
+      remove: ['HtmlWebpackPlugin'],
+      add: [
+        new HTMLWebpackPlugin(
+          Object.assign(
+            {},
+            {
+              hash: false,
+              filename: 'index.html',
+              template: path.resolve('public', 'index.html'),
+              inject: true,
+              chunks: ['main'],
+            },
+            true
+              ? {
+                  minify: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true,
+                    removeStyleLinkTypeAttributes: true,
+                    keepClosingSlash: true,
+                    minifyJS: true,
+                    minifyCSS: true,
+                    minifyURLs: true,
+                  },
+                }
+              : undefined
+          )
+        ),
+      ],
+    },
     configure: (webpackConfig, { env, paths }) => {
       return {
         ...webpackConfig,
