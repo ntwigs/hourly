007c8b319c01677cd44072a64025988d71db2e88
ntwigs <oscar.cv.nordquist@gmail.com>
2021-12-01 22:10:04 +0100

    chore: ignore exhaustive deps

--- src/hooks/use-should-render.ts
+++ src/hooks/use-should-render.ts
@@ -11,6 +11,7 @@ export const useShouldRender = ({ time = 0 }: Props): boolean => {
     setTimeout(() => {
       setShouldRender(true)
     }, time)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return shouldRender
