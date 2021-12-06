ae2812f04eec67950b65c0d68fc954b255d1701e
ntwigs <oscar.cv.nordquist@gmail.com>
2021-12-06 20:21:21 +0100

    feat: add intersection component

---
+++ src/components/intersect/index.tsx
@@ -0,0 +1,23 @@
+import { motion } from 'framer-motion'
+import { ReactNode, useState, useCallback } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+export const Intersect = ({ children }: Props): JSX.Element => {
+  const [inView, setInView] = useState(true)
+
+  const onViewportEnter = useCallback(() => setInView(true), [])
+  const onViewportLeave = useCallback(() => setInView(false), [])
+
+  return (
+    <>
+      <motion.div
+        onViewportEnter={onViewportEnter}
+        onViewportLeave={onViewportLeave}
+      />
+      {inView ? children : null}
+    </>
+  )
+}
