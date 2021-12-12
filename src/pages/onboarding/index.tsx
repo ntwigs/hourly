7c19be98926aa33fbf0d3506bb6f34f559d660a0
ntwigs <oscar.cv.nordquist@gmail.com>
2021-12-12 12:37:15 +0100

    feat: store onboarding finished

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,30 +1,58 @@
 import { Modal } from '@components/modal'
 import { onboard } from '@data/onboard'
-import { useCallback, useState } from 'react'
+import { useMount } from '@hooks/use-mount'
+import { storage } from '@utils/storage'
+import { AnimatePresence } from 'framer-motion'
+import { useCallback, useEffect, useState } from 'react'
+
+const useRenderModal = (): [
+  boolean | undefined,
+  (shouldRender: boolean) => void
+] => {
+  const [shouldRender, setShouldRender] = useState<boolean | undefined>(
+    undefined
+  )
+
+  useMount(() => {
+    storage.get('hasInitialized').then(({ hasInitialized }) => {
+      if (!hasInitialized) {
+        setShouldRender(true)
+      }
+    })
+  })
+
+  useEffect(() => {
+    if (shouldRender === false) {
+      storage.set({ hasInitialized: true })
+    }
+  }, [shouldRender])
+
+  return [shouldRender, setShouldRender]
+}
 
 export const Onboarding = () => {
   const [index, setIndex] = useState(0)
-  const [shouldRender] = useState(true)
+  const [shouldRender, setShouldRender] = useRenderModal()
   const onNext = useCallback(() => setIndex((index) => index + 1), [])
   const onBack = useCallback(() => setIndex((index) => index - 1), [])
-  const onClose = useCallback(() => setIndex((index) => index - 1), [])
-
-  if (!shouldRender) {
-    return null
-  }
+  const onClose = useCallback(() => setShouldRender(false), [setShouldRender])
 
   const { image, text, title } = onboard[index]
 
   return (
-    <Modal
-      title={title}
-      image={image}
-      text={text}
-      onNext={onNext}
-      onBack={onBack}
-      onClose={onClose}
-      isFirst={index === 0}
-      isLast={index === onboard.length - 1}
-    />
+    <AnimatePresence>
+      {shouldRender && (
+        <Modal
+          title={title}
+          image={image}
+          text={text}
+          onNext={onNext}
+          onBack={onBack}
+          onClose={onClose}
+          isFirst={index === 0}
+          isLast={index === onboard.length - 1}
+        />
+      )}
+    </AnimatePresence>
   )
 }
