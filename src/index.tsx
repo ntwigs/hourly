70a8c1581db3ff273f56753714ff964fefbc1678
ntwigs <oscar.cv.nordquist@gmail.com>
2021-12-12 12:38:04 +0100

    feat: use onboarding above start

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Start } from '@pages/start'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '@theme'
 import { GlobalStyle } from '@theme/global-style'
+import { Onboarding } from '@pages/onboarding'
+import { Start } from '@pages/start'
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Start />
+      <Onboarding />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
